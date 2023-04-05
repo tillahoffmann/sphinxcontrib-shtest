9ee26f2f535779d217652bc73626611a430c8627
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-04 22:47:42 -0400

    Require 100% test coverage.

--- recipe.py
+++ recipe.py
@@ -10,6 +10,7 @@ with create_group("build"):
     create_task("docs:shtest", action="rm -rf docs/_build && sphinx-build -b shtest . docs/_build")
     create_task("docs:html", action="sphinx-build -W . docs/_build")
     create_task("lint", action="flake8")
-    create_task("tests", action="pytest -v --cov=sphinxcontrib.shtest --cov-report=term-missing")
+    create_task("tests", action="pytest -v --cov=sphinxcontrib.shtest --cov-report=term-missing "
+                "--cov-report=html --cov-fail-under=100")
     sdist = create_task("package:sdist", action="python setup.py sdist")
     create_task("package:check", dependencies=[sdist], action="twine check --strict dist/*.tar.gz")
