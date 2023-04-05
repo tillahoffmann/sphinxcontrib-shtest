9ace7d56ac675132a7fb55881eb74eabe49c0703
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-04 22:48:17 -0400

    Add sphinx rst tests.

---
+++ tests/rst/true.rst
@@ -0,0 +1,3 @@
+.. shtest::
+
+    $ true
--- tests/test_shtest.py
+++ tests/test_shtest.py
@@ -1,6 +1,10 @@
 import colorama
+from pathlib import Path
 import pytest
+import shutil
+from sphinx.application import Sphinx
 from sphinxcontrib.shtest import ShTest, ShTestError, strip_colors
+from typing import Optional
 
 
 @pytest.mark.parametrize("test, match", [
@@ -22,3 +26,27 @@ def test_shtest(test: ShTest, match: str) -> None:
 def test_strip_colors() -> None:
     colored = f"{colorama.Fore.GREEN}hello {colorama.Fore.RED}world{colorama.Fore.RESET}"
     assert strip_colors(colored) == "hello world"
+
+
+@pytest.mark.parametrize("document, match", [
+    ("true", None),
+    ("false", "Expected return code: 0"),
+    ("false-1-tempdir", None),
+    ("multiple", None),
+    ("no-command", "Expected a command starting with"),
+    ("tempdir-and-cwd", "`cwd` and `tempdir` cannot"),
+    ("cwd", None),
+])
+def test_directive_build(tmp_path: Path, document: str, match: Optional[str]) -> None:
+    # Copy data to a temporary directory.
+    (tmp_path / "conf.py").write_text("extensions = ['sphinxcontrib.shtest']")
+    document = (Path(__file__).parent / "rst" / document).with_suffix(".rst")
+    shutil.copy(document, tmp_path / "index.rst")
+
+    # Run the builder.
+    app = Sphinx(tmp_path, tmp_path, tmp_path / "_build", tmp_path / "_toctree", "shtest")
+    if match:
+        with pytest.raises(ShTestError, match=match):
+            app.build()
+    else:
+        app.build()
