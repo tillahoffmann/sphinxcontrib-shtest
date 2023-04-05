2bc5e642b75ae48119ca87bf91e8c48a65905b4b
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-04 22:51:23 -0400

    Bump version to 0.3.0 and add url.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ long_description = long_description.replace(".. shtest::", ".. code-block::\n")
 
 setup(
     name="sphinxcontrib-shtest",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_namespace_packages(),
     install_requires=[
         "sphinx",
     ],
     long_description_content_type="text/x-rst",
     long_description=long_description,
+    url="https://github.com/tillahoffmann/sphinxcontrib-shtest",
 )
